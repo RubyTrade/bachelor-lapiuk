63839363bbbfc48364733b1130171e2a2ba01a7b
RickSanchezzz666 <116078373+RickSanchezzz666@users.noreply.github.com>
2026-05-16 20:12:18 +0300

    Update main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -23,18 +23,18 @@ resource "aws_key_pair" "my_key" {
   public_key = file("~/.ssh/kafka_ec2.pub")
 }
 
-# resource "aws_instance" "ec2" {
-#  ami = data.aws_ami.ubuntu.id   
-#  instance_type = "t3.small"
+resource "aws_instance" "ec2" {
+  ami = data.aws_ami.ubuntu.id   
+  instance_type = "t3.small"
+  
+  key_name      = aws_key_pair.my_key.key_name
+  vpc_security_group_ids = [data.aws_security_group.kafka_ec2_sg.id]
+  
+  tags = {
+    Name = "terraform-ec2"
+  }
+}
 
-#  key_name      = aws_key_pair.my_key.key_name
-#  vpc_security_group_ids = [data.aws_security_group.kafka_ec2_sg.id]
-
-#  tags = {
-#   Name = "terraform-ec2"
-#  }
-#}
-
-#output "public_ip" {
-#  value = aws_instance.ec2.public_ip
-#}
+output "public_ip" {
+  value = aws_instance.ec2.public_ip
+}
